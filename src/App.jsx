f52d791a4bab38daf429c57bb77d5f83af799d6a
Suraj Sunar <surbhu06@gmail.com>
2025-12-20 15:28:56 +0600

    app: added config changes to host in github

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Rcsc from "./pages/Rcsc";
 import Moice from "./pages/Moice";
 import Mofaet from "./pages/Mofae";
@@ -9,14 +9,14 @@ function App() {
   return (
     <div className="bg-amber-50">
       <div className="flex flex-col mx-auto h-screen w-1/2 items-center justify-center">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
-            <Route path="" Component={Home}></Route>
+            <Route path="/" Component={Home}></Route>
             <Route path="/rcsc" Component={Rcsc}></Route>
             <Route path="/moice" Component={Moice}></Route>
             <Route path="/mofaet" Component={Mofaet}></Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/rag/", // Replace 'your-repo-name'
+  base: "/govtech-agent/", // Replace 'your-repo-name'
 });
