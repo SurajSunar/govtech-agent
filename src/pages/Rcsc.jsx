144822cbebcbafa3c24f8569f7c71570a42ebd46
Suraj Sunar <surbhu06@gmail.com>
2025-12-25 14:29:53 +0600

    app: added files

--- src/pages/Rcsc.jsx
+++ src/pages/Rcsc.jsx
@@ -1,3 +1,5 @@
+import { BubbleChat } from "flowise-embed-react";
+
 const Rcsc = () => {
   return (
     <div className="bg-linear-to-br from-amber-200 via-amber-100 to-amber-50 w-full h-60 rounded-lg">
@@ -6,6 +8,10 @@ const Rcsc = () => {
           Welcome to the RAG Agent for RCSC
         </h1>
       </div>
+      <BubbleChat
+        chatflowid="fb1cd705-76ed-4db5-9baa-dc3dfa7b4e80"
+        apiHost="https://chatbot.tech.gov.bt"
+      />
     </div>
   );
 };
