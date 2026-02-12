d523e1039f8348505cc1a31848aa4ed463620dc9
Suraj Sunar <surbhu06@gmail.com>
2026-02-12 08:42:09 +0600

    app: change in config in moice

--- src/pages/Floatchat.jsx
+++ src/pages/Floatchat.jsx
@@ -1,14 +1,52 @@
 import { BubbleChat } from "flowise-embed-react";
 import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const Moe = () => {
+const APP_CONFIG = {
+  moe: {
+    appName: "MOE",
+    key: "9e9fb94f-0f14-4ee4-ad98-141d493e801b",
+    logo: "",
+  },
+  nlcs: {
+    appName: "NLCS",
+    key: "caf5e096-7ef7-406e-b213-a7b750783aac",
+    logo: "",
+  },
+  moh: {
+    appName: "MOH",
+    key: "a74fa02c-bc26-4275-a74b-e9e99cbe2de2",
+    logo: "",
+  },
+  moit: {
+    appName: "MOIT",
+    key: "ba354c53-d0ed-4d27-9d4d-3f2e9e795fa6",
+    logo: "",
+  },
+  moha: {
+    appName: "MOHA",
+    key: "fbbec566-f195-4590-8a7f-493badb57b91",
+    logo: "",
+  },
+  nsb: {
+    appName: "NSB",
+    key: "07789988-d360-4717-b597-7eca9ce63035",
+    logo: "",
+  },
+};
+
+const Floatchat = () => {
+  const { appName } = useParams();
+
+  const config = APP_CONFIG[appName];
+
   const theme = {
     disclaimer: {
       title: "Disclaimer",
       message: `<p style="text-align:justify">Responses are generated automatically by AI based on your input, and we cannot guarantee that the information will be complete, accurate, or up-to-date.
         <br/>Accordingly, the information provided by the Chatbot should not be considered as a basis for production workloads.
         <br/>You are solely responsible for the interactions and reliance on the information provided by the Chatbot, as well as for any actions taken or not taken.
-        <br/><b>AI-generated content may be inaccurate. Please validate before making decisions.</b></p>`,
+        <br/><b>AI-generated content may be for informational purpose. Please validate before making decisions.</b></p>`,
       textColor: "black",
       buttonColor: "#3b82f6",
       buttonText: "Start Chatting",
@@ -19,15 +57,15 @@ const Moe = () => {
       denyButtonBgColor: "#ef4444",
     },
     chatWindow: {
-      welcomeMessage:
-        "Kuzuzangpo la! 👋 I am MOE AI Assistant. How can I help you today?",
+      welcomeMessage: `Kuzuzangpo la! I am AI Assistant of the ${config?.appName}. How can I help you today?`,
       showTitle: true,
-      title: "MOE AI Chatbot",
+      title: `${config?.appName} Chatbot`,
       titleAvatarSrc: "https://tech.gov.bt/wp-content/uploads/2025/09/logo.png",
       botMessage: {
         showAvatar: true,
         avatarSrc:
-          "https://rcsc.gov.bt/wp-content/uploads/2024/11/RCSC-logo-1.png",
+          config?.logo ||
+          "https://tech.gov.bt/wp-content/uploads/2025/09/logo.png",
       },
       footer: {
         textColor: "#303235",
@@ -44,15 +82,26 @@ const Moe = () => {
       "chatbotDisclaimer=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
   }, []);
 
+  if (!config) {
+    return (
+      <>
+        <p className="text-2xl text-amber-800">No such Agency</p>
+        <Link to={"/"} className="underline text-amber-600">
+          Back to Home
+        </Link>
+      </>
+    );
+  }
+
   return (
     <div className="bg-linear-to-br from-amber-200 via-amber-100 to-amber-50 w-full h-60 rounded-lg">
       <div className="h-full flex justify-center items-center">
-        <h1 className="text-4xl text-amber-800">
-          Welcome to the RAG Agent for MOE
+        <h1 className="text-4xl text-amber-700">
+          Welcome to the AI Chatbot for {config?.appName}
         </h1>
       </div>
       <BubbleChat
-        chatflowid="fb1cd705-76ed-4db5-9baa-dc3dfa7b4e80"
+        chatflowid={config?.key}
         apiHost="https://chatbot.tech.gov.bt"
         theme={{ ...theme }}
       />
@@ -60,4 +109,4 @@ const Moe = () => {
   );
 };
 
-export default Moe;
+export default Floatchat;
